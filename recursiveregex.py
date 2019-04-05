73257bf02c5bbd5d673b89b3a8e0e02eb17ceb96
Tim Zwart <Tim.Zwart@vixion.nl>
2019-04-05 09:54:12 +0200

    this works

--- recursiveregex.py
+++ recursiveregex.py
@@ -2,22 +2,29 @@
 import re
 import argparse
 import os
-
+import pdb
 
 parser = argparse.ArgumentParser()
 parser.add_argument("pattern")
+parser.add_argument("replacement")
+arguments = parser.parse_args()
 
 folder = os.getcwd()
 
+
 for (dirpath, dirnames, filenames) in os.walk(folder):
     for filename in filenames:
-        opened = open(filepath, 'rw')
+        filepath = "/".join([dirpath , filename])
+        opened = open(filepath)
         str_f = opened.read()
+        opened.close()
         lines = str_f.split('\n')
         newlines = []
         for line in lines:
-            re.sub(pattern, line)
-            newlines.append(line)
-        newstr = lines.join('\n')
-        opened.write(newstr) 
+            newline = re.sub(arguments.pattern, arguments.replacement, line)
+            newlines.append(newline)
+        newstr = '\n'.join(newlines)
+        opened = open(filepath, 'w')
+        opened.write(newstr)
+        opened.close() 
 
